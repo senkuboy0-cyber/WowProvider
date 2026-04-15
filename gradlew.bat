10e38920a359e2fb7afaf19a8de3ae8f949f32a9
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-15 09:14:00 +0600

    Update gradlew.bat script

--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,16 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
-@rem                                                                         ##
-@rem  Gradle startup script for Windows                                      ##
-@rem                                                                         ##
+@rem
+@rem  Gradle startup script for Windows
+@rem
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%" == "Windows_NT" setlocal
 
-set DIRNAME=%~dp0%
+set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0%
+set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
@@ -27,7 +27,7 @@ echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+location of your Java installation.
 
 goto fail
 
@@ -41,7 +41,7 @@ echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+location of your Java installation.
 
 goto fail
 
@@ -75,11 +75,16 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
-:endLocal
-if "%OS%" == "Windows_NT" endlocal
+:end
+@if "%ERRORLEVEL%" == "0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable GRADLE_EXITCONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-cmd /c exit /b 1
+exit /b 1
+
+:mainEnd
+if "%OS%" == "Windows_NT" endlocal
+
+:omega
