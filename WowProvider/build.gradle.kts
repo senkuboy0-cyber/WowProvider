59b31178b8fbf4b5e79d80d92e900c4b4ae74471
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-15 12:56:57 +0600

    Add build.gradle.kts for WowProvider module

---
+++ WowProvider/build.gradle.kts
@@ -0,0 +1,12 @@
+cloudstream {
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/senkuboy0-cyber/WowProvider")
+    version = 1
+    description = "Wow.xxx - Free Porn Videos"
+    authors = listOf("senkuboy0-cyber")
+    language = "en"
+    tvTypes = listOf("Movie")
+}
+
+android {
+    namespace = "com.wow"
+}
